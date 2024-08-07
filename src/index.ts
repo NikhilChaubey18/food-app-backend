c6d6bbe1e4e92f687167dea35dd88724f2b9250f
NikhilChaubey18 <nikhilch1804@gmail.com>
2024-08-07 10:22:57 +0530

    adding feature

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute"
+import  myRestaurantRoute from "./routes/MyRestaurantRoutes"
+import {v2 as cloudinary} from"cloudinary";
 
 mongoose.connect(process.env.MONGODB_URL as string).then(() => {
   console.log("your are connected to data base")
+});
+
+cloudinary.config({
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET,
 })
 
 
@@ -20,6 +28,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute)
+app.use("/api/my/restaurant", myRestaurantRoute)
 
 
   app.listen(8080, () => {
