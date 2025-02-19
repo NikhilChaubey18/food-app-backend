bd07ebeefa0cbdf08531f5f70258ccf417b390bc
NikhilChaubey18 <nikhilch1804@gmail.com>
2025-02-19 16:38:45 +0530

    latest commit

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -4,6 +4,16 @@ import { param } from "express-validator";
 import RestaurantController from "../controllers/RestaurantController";
 const router = express.Router();
 
+router.get(
+    "/:restaurantId",
+    param("restaurantId")
+      .isString()
+      .trim()
+      .notEmpty()
+      .withMessage("RestaurantId paramenter must be a valid string"),
+    RestaurantController.getRestaurant
+  );
+
 router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("City parameter must be a valid string"),RestaurantController.searchRestaurants)//validation
 
 
